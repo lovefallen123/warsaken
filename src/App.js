edcfe032245b91df472a3ed875d4df741e2eba61
Alvin Lee <alvin_lee@mymail.sutd.edu.sg>
2022-11-11 16:55:12 +0800

    added local storage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import URL from './URL.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  async loadJsonURL(url,key) {
+    var response=fetch(url.stringify(), {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+          },
+      })
+      .then(response => response.json())
+      .then(response => JSON.stringify(response))
+      var jsonresponse = await response
+      localStorage.setItem(key,jsonresponse);
+      console.log("Fetch from API completed");
+      this.populateTemplate();
+  }
+
+  async populateTemplate(){
+    if ("templateJson" in localStorage){
+      let jsonresponse=localStorage.getItem("templateJson")
+      jsonresponse = JSON.parse(jsonresponse)
+      let rank = new Set()
+      let rarity = new Set()
+      let type = new Set()
+      let subset = new Set()
+      jsonresponse.data.forEach(element => {
+          rank.add(element.immutable_data.rank)
+          rarity.add(element.immutable_data.rarity)
+          type.add(element.immutable_data.type)
+          subset.add(element.immutable_data.subset)
+        });
+        rank.delete(undefined)
+        rarity.delete(undefined)
+        type.delete(undefined)
+        subset.delete(undefined)
+
+        this.setState({
+          rank : [...rank],
+          rarity : [...rarity],
+          type : [...type],
+          subset : [...subset]},()=>{console.log("state set");console.log(this.state)}
+        )
+    }else{
+      console.log("empty")
+      console.log(this.state)
+    }
+    
+  }
+
+  constructor(props){
+    super(props);
+    const templatelink = new URL("https://wax.api.atomicassets.io/atomicassets/v1/templates")
+    templatelink.add({
+      collection_name:"warsaken",
+      limit:1000,
+      order:"desc",
+      sort:"created"
+    });
+    this.state={
+        templatelink:templatelink
+    }
+  }
+
+componentDidMount(){
+  this.populateTemplate();
+}
+  static templatelink ="https://wax.api.atomicassets.io/atomicassets/v1/templates";
+  static collection_name="warsaken";
+  static expdict = { };
+  static namelist=[];
+  static raritylist=[];
+  render()  {
+      return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <button onClick={()=>this.loadJsonURL(this.state.templatelink,'templateJson')}>
+            loadTemplate
+          </button>
+          <button onClick={()=>this.populateTemplate()}>
+            printTemplate
+          </button>
+          <button onClick={()=>localStorage.clear()}>
+            clearCache
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
